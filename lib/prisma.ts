21658cedfda288cb4c95196c5ab2bcf68dd38904
theNubiadev <ajaiyeobajibola@gmail.com>
2026-03-30 02:32:34 +0100

    fix(prisma): remove output path from client generator in schema.prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,30 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+
+// const connectionString = `${process.env.DATABASE_URL}`;
+
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export { prisma };
+
+//import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
