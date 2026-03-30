33acd678492d0e6a6a2459a34749343d96e79472
theNubiadev <ajaiyeobajibola@gmail.com>
2026-03-30 02:53:41 +0100

    refactor(prisma): clean up unused code and imports in prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,3 @@
-// import "dotenv/config";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { PrismaClient } from "@prisma/client";
-
-// const connectionString = `${process.env.DATABASE_URL}`;
-
-// const adapter = new PrismaPg({ connectionString });
-// const prisma = new PrismaClient({ adapter });
-
-// export { prisma };
-
-//import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
